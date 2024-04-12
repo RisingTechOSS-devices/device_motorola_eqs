63e4fedf39da13a6b656a7530580c0ab77e8936c
Davigamer987 <davimarchesiprimo@gmail.com>
2024-04-12 01:03:07 -0300

    eqs: Remove Lineage HALS
    Hals are broken with 0x20, will fix later
    
    Change-Id: I58f42bf9b0777ca0681a60b69181d09a6300882c

--- device.mk
+++ device.mk
@@ -82,14 +82,6 @@ PRODUCT_COPY_FILES += \
 # Quick Tap
 PRODUCT_PACKAGES += libtensorflowlite_jni
 
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.eqs
-
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.eqs
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
