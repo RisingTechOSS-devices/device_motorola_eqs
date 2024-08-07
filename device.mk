b28548f95ea7a13e0326a1ef7be0bc45bf15a581
davigamer987 <davimarchesiprimo@gmail.com>
2024-08-08 01:27:56 +0200

    eqs: Move V4a blobs to common tree

--- device.mk
+++ device.mk
@@ -51,11 +51,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
     $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
-# AudioFX-Viper4Android-RE
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/ViperFX/audio_effects.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/prebuilt/lib64/soundfx/libv4a_re.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libv4a_re.so
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.moto_sm8475_fod
